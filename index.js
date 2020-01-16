a05090e840b53d438440a82b79cb66b9bfce63bc
Ronald C Edwards <rce246@stern.nyu.edu>
2020-01-16 16:24:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ function add(a,b){return a + b}
 function subtract(a,b) {return a - b}
 function multiply(a,b) {return a * b}
 function divide(a,b) {return a / b}
-function inc(n) {var n return n++}
-function dec(n) {var n return n--}
+function inc(n) {return n+= 1}
+function dec(n) {return n-= 1}
